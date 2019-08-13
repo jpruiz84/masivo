e4a20cea28aaf80b73763f4821b7385618202a86
Juan Ruiz-Rosero <jpabloruiz@unicauca.edu.co>
2019-08-13 09:20:53 -0500

    Organized test input files

--- globalConstants.py
+++ globalConstants.py
@@ -7,13 +7,16 @@ stops_name_to_num = {}
 # Graph configuration constants
 BUS_AND_STOPS_SCALE = 20
 PASS_TOTAL_ARRIVAL_TIME = 3600     # In secs
-LOGGING_LEVEL = logging.INFO
-if 1:
-  ODM_FILE = 'utils/odmTest.csv'
-  ROUTES_FILE = 'utils/routesTest.csv'
-else:
-  ODM_FILE = 'utils/odm1.csv'
-  ROUTES_FILE = 'utils/routes1.csv'
+LOGGING_LEVEL = logging.ERROR
+
+test_scenario = 30
+
+if test_scenario == 3:
+  ODM_FILE = 'utils/odm3.csv'
+  ROUTES_FILE = 'utils/routes3.csv'
+if test_scenario == 30:
+  ODM_FILE = 'utils/odm30.csv'
+  ROUTES_FILE = 'utils/routes30.csv'
 
 # Masivo fixed constants, DO NOT MODIFY !!!!
 PASS_DATA_FORMAT = 'HHHHL'     # (alight_time, arrival_time, dest_stop, orig_stop, pass_id)
