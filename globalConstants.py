d2b2e0303a31ccd56a787df8285b044c300f4bb8
Juan Ruiz-Rosero <jpabloruiz@unicauca.edu.co>
2019-08-16 08:39:50 -0500

    Added case 300 stops

--- globalConstants.py
+++ globalConstants.py
@@ -6,10 +6,10 @@ stops_name_to_num = {}
 
 # Graph configuration constants
 BUS_AND_STOPS_SCALE = 20
-PASS_TOTAL_ARRIVAL_TIME = 3600     # In secs
-LOGGING_LEVEL = logging.ERROR
+PASS_TOTAL_ARRIVAL_TIME = 0     # In secs
+LOGGING_LEVEL = logging.INFO
 
-test_scenario = 30
+test_scenario = 300
 
 if test_scenario == 3:
   ODM_FILE = 'utils/odm3.csv'
@@ -17,6 +17,10 @@ if test_scenario == 3:
 if test_scenario == 30:
   ODM_FILE = 'utils/odm30.csv'
   ROUTES_FILE = 'utils/routes30.csv'
+if test_scenario == 300:
+  ODM_FILE = 'utils/odm300.csv'
+  ROUTES_FILE = 'utils/routes300.csv'
+
 
 # Masivo fixed constants, DO NOT MODIFY !!!!
 PASS_DATA_FORMAT = 'HHHHL'     # (alight_time, arrival_time, dest_stop, orig_stop, pass_id)
