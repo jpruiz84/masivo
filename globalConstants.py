2523d91af8926e6bfbf4b6eeba295446f09cad13
Juan Ruiz-Rosero <jpabloruiz@unicauca.edu.co>
2019-08-19 10:00:39 -0500

    Added pass np type

--- globalConstants.py
+++ globalConstants.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 
 # Global variables
 pass_num = 0
@@ -24,4 +25,15 @@ if test_scenario == 300:
 
 # Masivo fixed constants, DO NOT MODIFY !!!!
 PASS_DATA_FORMAT = 'HHHHL'     # (alight_time, arrival_time, dest_stop, orig_stop, pass_id)
+PASS_TYPE = np.dtype([('pass_id', 'u4'), ('orig_stop', 'u2'), ('dest_stop', 'u2'),
+                      ('arrival_time', 'u2'), ('alight_time', 'u2'), ('status', 'u1')])
 STOP_BUS_WINDOW_DISTANCE = 10
+
+
+STOP_MAX_PASS = 8000
+
+PASS_STATUS_EMPTY = 0
+PASS_STATUS_TO_ARRIVE = 1
+PASS_STATUS_ARRIVED = 2
+PASS_STATUS_IN_BUS = 3
+PASS_STATUS_ALIGHTED = 4
