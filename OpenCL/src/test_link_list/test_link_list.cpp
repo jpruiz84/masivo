56a382e1a5e12f86a13d7e378ca20be59d99f764
Juan Ruiz-Rosero <jpabloruiz@unicauca.edu.co>
2019-09-05 00:51:18 -0500

    Fill stopQueue list from arrival list

--- OpenCL/src/test_link_list/test_link_list.cpp
+++ OpenCL/src/test_link_list/test_link_list.cpp
@@ -3,8 +3,8 @@
 #include <time.h>       /* time */
 #include <string.h>
 
-#define STOPS_NUM                    3
-#define STOP_MAX_PASS               10
+#define STOPS_NUM                    300
+#define STOP_MAX_PASS               10000
 #define SIM_TIME                  6000     // In secs
 #define PASS_TOTAL_ARRIVAL_TIME   3600     // In secs
 
@@ -82,7 +82,7 @@ listIsEmpty(
   LIST_HT *listHt
   )
 {
-  return (listHt->tail == NULL);
+  return (listHt->tail == NULL || listHt->head == NULL);
 }
 
 LIST_ENTRY*
@@ -108,6 +108,21 @@ listIsTheLast(
 {
   return(Node->next == NULL);
 }
+
+LIST_ENTRY*
+listPop(
+  LIST_HT *listHt
+  )
+{
+
+  LIST_ENTRY* popEntry;
+
+  popEntry = listHt->head;
+  listHt->head = listHt->head->next;
+
+  return popEntry;
+}
+
 
 
 int
@@ -137,7 +152,7 @@ listSortByArrivalTime(
   LIST_HT *listHt
   )
 {
-    int swapped;
+  int swapped;
   int i;
   LIST_ENTRY *ptr1;
   LIST_ENTRY *lptr = NULL;
@@ -175,6 +190,7 @@ main()
 {
   LIST_ENTRY *node;
   PASS_TYPE *passEntry = NULL;
+  clock_t procTime;
 
   printf("\fStarting test link list.\n");
 
@@ -258,6 +274,85 @@ main()
     }while(node);
   }
 #endif
+
+
+
+
+#if 1
+
+  procTime = clock();
+
+  for (unsigned int sim_time = 0; sim_time < SIM_TIME; ++sim_time) {
+    if((sim_time % 1) == 0){
+      printf("\rtime: %d", sim_time);
+
+    }
+
+    for (unsigned int i = 0; i < STOPS_NUM; ++i) {
+      if(stopsArrival[i].total > 0){
+        while(sim_time == PASS_FROM_THIS(stopsArrival[i].listHt.head)->arrivalTime){
+
+          listInsert(&stopsQueue[i].listHt, listPop(&stopsArrival[i].listHt));
+          stopsArrival[i].total --;
+          stopsQueue[i].total ++;
+
+          if(stopsArrival[i].total == 0){
+            break;
+          }
+        }
+      }
+    }
+
+  }
+
+  procTime = clock() - procTime;
+#endif
+
+
+
+#if PRINT_LIST
+  for (unsigned int i = 0; i < STOPS_NUM; ++i) {
+    printf("\nstopsArrival[%d], head: 0x%08X, tail: head: 0x%08X\n",
+           i, stopsArrival[i].listHt.head, stopsArrival[i].listHt.tail);
+
+    if(listIsEmpty (&stopsArrival[i].listHt)){
+      break;
+    }
+
+    node = listGetFirstNode(&stopsArrival[i].listHt);
+    do {
+      passEntry = PASS_FROM_THIS(node);
+      printf("Stop %d, passId %d, origStop %d, destStop %d, arrivalTime %d, alightTime %d, next: %d\n",
+        i, passEntry->passId, passEntry->origStop, passEntry->destStop,
+        passEntry->arrivalTime, passEntry->alightTime, passEntry->listEntry.next);
+
+      node = listGetNextNode(node);
+
+    }while(node);
+  }
+
+  for (unsigned int i = 0; i < STOPS_NUM; ++i) {
+    printf("\nstopsQueue[%d], head: 0x%08X, tail: head: 0x%08X\n",
+           i, stopsQueue[i].listHt.head, stopsQueue[i].listHt.tail);
+
+    if(listIsEmpty (&stopsQueue[i].listHt)){
+      break;
+    }
+
+    node = listGetFirstNode(&stopsQueue[i].listHt);
+    do {
+      passEntry = PASS_FROM_THIS(node);
+      printf("Stop %d, passId %d, origStop %d, destStop %d, arrivalTime %d, alightTime %d, next: %d\n",
+        i, passEntry->passId, passEntry->origStop, passEntry->destStop,
+        passEntry->arrivalTime, passEntry->alightTime, passEntry->listEntry.next);
+
+      node = listGetNextNode(node);
+
+    }while(node);
+  }
+#endif
+
+  printf("Elapsed: %f seconds\n", (double)(procTime) / CLOCKS_PER_SEC);
 
   return 0;
 }
