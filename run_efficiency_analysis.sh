e5293eff26c7411b4c6917444024c4fb71738107
Juan Ruiz-Rosero <jpabloruiz@unicauca.edu.co>
2019-11-10 20:00:22 -0500

    Updated run efficiency to get automatically number of threads

--- run_efficiency_analysis.sh
+++ run_efficiency_analysis.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# Setting max performance
+N_THREADS="$(nproc --all)"
+echo "Total threads: $N_THREADS"
+
+for ((i=0; i<=N_THREADS -1; i++)); do
+   echo performance > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+done
+
+cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
 # Compile the c code
 gcc -shared -Wl,-soname, -o c_code/masivo_c.so -fPIC c_code/masivo_c.c
 
 rm -fr results*/
 
-for i in {8..0}
-do
-   sleep 10
+for ((i=0; i<=N_THREADS; i++)); do
    echo "******** RUNNING LIMITED TO:  $i CU"
    python3 main.py -m $i
    mv results results_$i
